49ca75125f25da4211f011f18c726875631d8757
sagarrao1 <sagarrao1@gmail.com>
2019-06-06 19:12:39 +0530

    Hibernate CORE  JAVA PROJECT DEMO

---
+++ Hibernate_Core/src/main/java/com/learning/Hibernate_Core/App.java
@@ -0,0 +1,15 @@
+
+package com.learning.Hibernate_Core;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Student s1 = new Student();
+        s1.setId(101);
+        s1.setName("Sagar");
+        s1.setPoints(79);
+    }
+}
---
+++ Hibernate_Core/src/main/java/com/learning/Hibernate_Core/Student.java
@@ -0,0 +1,39 @@
+package com.learning.Hibernate_Core;
+
+public class Student {
+	private int id;
+	private String name;
+	private  int points;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", points=" + points + "]";
+	}	
+	
+	
+
+}
