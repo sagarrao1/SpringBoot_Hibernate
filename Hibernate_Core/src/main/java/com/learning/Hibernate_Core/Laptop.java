3443aac92434a86db44d93be04826f1b88161791
sagarrao1 <sagarrao1@gmail.com>
2019-06-11 21:48:07 +0530

    video 15

---
+++ Hibernate_Core/src/main/java/com/learning/Hibernate_Core/Laptop.java
@@ -0,0 +1,48 @@
+package com.learning.Hibernate_Core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Laptop {
+	
+	@Id
+	private int lid;
+	private String lname;
+	@ManyToMany
+	private List<Student> students = new ArrayList<Student>();
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+	
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+	@Override
+	public String toString() {
+		return "Laptop [lid=" + lid + ", lname=" + lname + "]";
+	}
+	
+	
+	
+
+}
