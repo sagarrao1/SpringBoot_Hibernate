2016ddab01d77685e577ebe968fd7ecc20bfd97d
sagarrao1 <sagarrao1@gmail.com>
2019-06-14 19:02:17 +0530

    till Video 22

--- Hibernate_Core/src/main/java/com/learning/Hibernate_Core/App3.java
+++ Hibernate_Core/src/main/java/com/learning/Hibernate_Core/App3.java
@@ -1,8 +1,13 @@
 package com.learning.Hibernate_Core;
 
+import java.util.List;
+import java.util.Random;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
 
 public class App3 {
 
@@ -15,17 +20,36 @@ public class App3 {
 		 * Alien_table a2 = new Alien_table(); a2.setAid(2); a2.setAname("Ravi");
 		 * a2.setMarks(16);
 		 */		
+		Alien_table at =new Alien_table();
 		
 		Configuration config = new Configuration().configure().addAnnotatedClass(Alien_table.class);		
 		SessionFactory sf = config.buildSessionFactory();
 		
-		System.out.println("session 1 ................");
+		
 		Session session1 = sf.openSession();
 		session1.beginTransaction();
 		
-		Alien_table an1 = (Alien_table) session1.get(Alien_table.class, 1);
-		System.out.println(an1);
+		System.out.println("session 1 ................");
 		
+		/*
+		 * Random r= new Random();
+		 * 
+		 * for (int i = 3; i < 100; i++) { Alien_table t = new Alien_table();
+		 * t.setAid(i); t.setAname("Sagar "+i); t.setMarks(r.nextInt(100));
+		 * session1.save(t); }
+		 */		
+
+		// Query with cache Query q1=
+		Query q1=session1.createQuery("select aid ,aname,marks from Alien_table where marks>80");
+		q1.setCacheable(true);
+		List<Object[]> students = q1.list();
+
+		// at= (Alien_table) q1.uniqueResult(); //Alien_table at = (Alien_table)
+		session1.get(Alien_table.class, 1);
+		for (Object[] objects : students) {
+			System.out.println(objects[0] + " : " + objects[1] + " : " + objects[2]);
+		}
+ 				
 		session1.getTransaction().commit();
 		session1.close();
 		
@@ -33,8 +57,14 @@ public class App3 {
 		Session session2 = sf.openSession();
 		session2.beginTransaction();
 		
-		an1 =  (Alien_table) session2.get(Alien_table.class, 1);
-		System.out.println(an1);
+		//Query with cache
+		Query q2= session2.createQuery("select aid ,aname,marks from Alien_table where aid=2");
+		q2.setCacheable(true);
+		Object[] std= (Object[]) q2.uniqueResult();	
+		System.out.println(std[0]+" : "+std[1]+" : "+std[2]);
+				
+		//at =  (Alien_table) session2.get(Alien_table.class, 1);
+		//System.out.println(at);
 		
 		session2.getTransaction().commit();
 		session2.close();
