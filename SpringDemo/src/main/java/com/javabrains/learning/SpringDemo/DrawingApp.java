a3191d81487eda7584349631d672451f3261b7f3
sagarrao1 <sagarrao1@gmail.com>
2019-06-30 19:57:30 +0530

    Spring Demo by java brains koushik

---
+++ SpringDemo/src/main/java/com/javabrains/learning/SpringDemo/DrawingApp.java
@@ -0,0 +1,15 @@
+package com.javabrains.learning.SpringDemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
+		Shape shape= (Shape) context.getBean("shape");
+		shape.draw();
+
+	}
+
+}
