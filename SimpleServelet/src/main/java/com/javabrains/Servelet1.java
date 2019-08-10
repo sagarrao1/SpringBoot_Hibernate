716237431bf79b4d0472a8a9535c139fb0dd6e7d
sagarrao1 <sagarrao1@gmail.com>
2019-08-10 20:42:38 +0530

    Simple servelet till video 16

--- SimpleServelet/src/main/java/com/javabrains/Servelet1.java
+++ SimpleServelet/src/main/java/com/javabrains/Servelet1.java
@@ -3,13 +3,18 @@ package com.javabrains;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet(description = "a Servelet-path description", urlPatterns = "/Servelet-path1")
+//@WebServlet(description = "a Servelet-path description", urlPatterns = "/Servelet-path1",
+//  initParams = {@WebInitParam(name = "DefaultUser", value = "Sandhya Darling")}  
+//		)
 public class Servelet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -19,8 +24,24 @@ public class Servelet1 extends HttpServlet {
 		PrintWriter writer=  response.getWriter();
 		System.out.println("Hello world from doGet servelet1");
 		writer.println("<h4> Hello world from doGet servelet1</h4>");
-		String userName= request.getParameter("userName");
-		writer.println("<h1> Hello "+userName+"</h4>");		
+		String userName= request.getParameter("name");
+		HttpSession session= request.getSession();
+		ServletContext context=request.getServletContext();
+		
+		if (userName !="" & userName != null) {
+		session.setAttribute("saveUserName", userName);
+		context.setAttribute("saveCtxUserName", userName);
+		}
+		
+		
+		writer.println("Username from request Object: "+userName+"<br>");
+		writer.println("Username from session Object: "+(String)session.getAttribute("saveUserName")+"<br>");
+		writer.println("Username from context Object: "+(String)context.getAttribute("saveCtxUserName")+"<br>");
+		
+		writer.println("Default Username from init Parameter: "+this.getServletConfig().getInitParameter("DefaultUser")+"<br>");
+		
+		
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
@@ -28,9 +49,20 @@ public class Servelet1 extends HttpServlet {
 	  response.getWriter().append("Served at: ").append(request.getContextPath());
 	  PrintWriter writer= response.getWriter();
 	  System.out.println("Hello world from doPOST servelet1");
-	  writer.println("<h4> Hello world from doPOST servelet1</h4>"); String
-	  userName= request.getParameter("userName");
-	  writer.println("<h1> Hello from doPOST : "+userName+"</h4>"); 
+	  //writer.println("<h4> Hello world from doPOST servelet1</h4>"); 
+	  String userName= request.getParameter("userName");
+	  String fullName= request.getParameter("fullName");
+	  writer.println("<h4> Hello from doPOST : "+userName+" and your full name is : "+fullName+"</h4>"); 
+	  
+	  String prof= request.getParameter("prof");
+	  writer.println("You are a : "+prof+"<br><br>");	  
+	  
+	  String[] location= request.getParameterValues("location");
+	  writer.println("Your location is : <br>");
+	  for (String string : location) {
+		  writer.println(string+"<br>");
+	}
+	  
 	}
 	
 }
--- SimpleServelet/src/main/webapp/index.jsp
+++ SimpleServelet/src/main/webapp/index.jsp
@@ -1,5 +1,36 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<br>
+<%! public int add(int a, int b){
+	return a+b;
+}
+%>
+
+<%
+  int i=2;
+  int j=4;
+  int k= i+j;
+  
+  //out.println("Value of k is :"+k);
+%>
+
+Value of k is: <%=k%>
+
+<% k= add(2345, 5678);
+%>
+<br>
+Value of k is: <%=k%>
+<br>
+<%
+ for(i=0;i<10;i++){
+//	 out.println("value of i is :"+i+"<br>");
+ 
+%>
+  value of i is :<%=i %><br>
+
+<%}
+%>
+
 </body>
 </html>
